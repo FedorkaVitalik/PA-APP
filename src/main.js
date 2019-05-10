522b1a792d1b52d6962527e4012bfdf7bce7d378
Vitalij Fedorka <vitaljchik@gmail.com>
2019-05-10 09:15:30 +0300

    install vuetify and confirmation from vuetify

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import Vue from 'vue';
+import Vuetify from 'vuetify';
+import VuetifyConfirm from 'vuetify-confirm';
+import 'vuetify/dist/vuetify.min.css';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+Vue.use(Vuetify);
+
+Vue.use(VuetifyConfirm, {
+  buttonTrueText: 'Ok',
+  buttonFalseText: 'Cancel',
+  width: 400,
+});
 
 Vue.config.productionTip = false;
 

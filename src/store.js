712e5736b2aca2291c7b70edde8d9e750ff8b688
Vitalij Fedorka <vitaljchik@gmail.com>
2019-05-10 09:09:31 +0300

    prepare store

--- src/store.js
+++ src/store.js
@@ -1,16 +1,16 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import user from './store/user';
+import general from './store/general';
+import posts from './store/posts';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
+  modules: {
+    user,
+    general,
+    posts,
   },
 });

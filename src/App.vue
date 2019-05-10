b973745003030124491777496cdf8e998d9c1e51
Vitalij Fedorka <vitaljchik@gmail.com>
2019-05-10 16:46:35 +0300

    add footer

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,18 @@
     <v-content>
       <router-view />
     </v-content>
+    <app-footer></app-footer>
   </v-app>
 </template>
 
 <script>
 import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 export default {
   components: {
-    'app-header': Header
+    'app-header': Header,
+    'app-footer': Footer
   }
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <v-footer :fixed="true" app>
+      <span>My Blog &copy;2019</span>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
